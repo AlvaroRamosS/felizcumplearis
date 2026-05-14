7d6ce8848d5ec9e1f81174c529c2e9d11f77f6a7
AkaiRamone <alvaro.ramos.s@outlook.com>
2026-05-13 18:15:27 -0600

    florecitas

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-
-
 $(document).ready(function () {
-    
-    $("#thankyou").click(function (e) { 
-        e.preventDefault();
-        alert("De nada uwu");
-        
-    });
+  $("#thankyou").click(function (e) {
+    e.preventDefault();
+    alert("De nada tonoto");
+  });
 
-    $("#sorpresa").click(function (e) { 
-        
-        alert("Pero espera! Eso no es todo! Si le dices a Álvaro el código secreto 'Me pica la cola' serás ganadora de una rascadita gratis! Por cierto te amo c:");
-        showMenu(true);
-    });
-
-
+  $("#sorpresa").click(function (e) {
+    alert(
+      "Pero espera! Eso no es todo! Si le dices a Álvaro el código secreto 'Me pica la cola' serás ganadora de una rascadita gratis! Por cierto te amo c:",
+    );
+    showMenu(true);
+  });
 });
